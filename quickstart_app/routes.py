46690bafa9eb39ffc1fe63793fe773c52b35307b
EliasKomar <elias.komar@ctemplar.com>
2020-07-07 21:34:38 +0200

    rebuilding old pages

--- quickstart_app/routes.py
+++ quickstart_app/routes.py
@@ -1,7 +1,7 @@
 from flask_user import login_required, UserManager
 from quickstart_app import app, db
-from flask import render_template
-from quickstart_app.models import User
+from flask import render_template, request
+from quickstart_app.models import User, Task
 
 user_manager = UserManager(app, db, User)   # Setup Flask-User and specify the User data-model
 
@@ -14,4 +14,10 @@ def index():
 @app.route('/schedule')
 @login_required    # User must be authenticated
 def schedule():
-    return render_template('schedule.html', schedule='')
+    return render_template('schedule.html', schedule=Task.query.all())
+
+@app.route('/task')
+@login_required    # User must be authenticated
+def task():
+    task = Task.query.get(request.args.get('id'))
+    return render_template('task.html', task=task)
