e3d1a0e7ef1374a41138608fb2dd83e5140940ac
EliasKomar <elias.komar@ctemplar.com>
2020-09-14 22:07:46 +0200

    removed flask-user

--- quickstart_app/routes.py
+++ quickstart_app/routes.py
@@ -3,14 +3,11 @@ from quickstart_app.tools import allowed_file, log, make_admin, add_subject
 from quickstart_app.Forms import RegistrationForm, LoginForm
 from quickstart_app import app, db
 from flask import render_template, request, redirect, url_for, send_from_directory, abort, flash
-from flask_login import current_user
-from flask_user import login_required, UserManager
 from werkzeug.utils import secure_filename
 from datetime import datetime
 import os
 
-user_manager = UserManager(app, db, User)   # Setup Flask-User and specify the User data-model
-
+# TODO: alternative to current_user
 
 @app.route('/')
 @app.route('/home')
@@ -18,7 +15,6 @@ def index():
     return render_template('index.html')
 
 @app.route('/schedule')
-@login_required
 def schedule():
     return render_template('schedule.html', schedule=Task.query.all())
 
@@ -42,7 +38,6 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 @app.route('/task/<task_id>')
-@login_required
 def task(task_id):
     task  = Task.query.get(task_id)
     subject = Subject.query.get(task.subject_id)
@@ -51,7 +46,6 @@ def task(task_id):
     return render_template('task.html', task=task, subject=subject)
 
 @app.route('/add_comment/<task_id>', methods=['GET', 'POST'])
-@login_required    # User must be authenticated
 def add_comment(task_id):
     if request.method == 'POST':
         db.session.add(Comment(title=request.form['title'], comment=request.form['comment'], user_id=current_user.id, schedule_id=task_id))
@@ -61,7 +55,6 @@ def add_comment(task_id):
     return render_template('add_comment.html')
 
 @app.route('/add_task', methods=['GET', 'POST'])
-@login_required    # User must be authenticated
 def add_task():
     if request.method == 'POST':
         db.session.add(Task(name=request.form['name'], description=request.form['description'], deadline=datetime.strptime(request.form['deadline'], '%d.%m.%Y'), user_id=current_user.id, subject_id=request.form['subject']))
@@ -71,7 +64,6 @@ def add_task():
     return render_template('add_task.html', subjects=Subject.query.all())
 
 @app.route('/upload_file/<task_id>', methods=['GET', 'POST'])
-@login_required    # User must be authenticated
 def upload_file(task_id):
     if request.method == 'POST':
         # check if the post request has the file part
@@ -94,7 +86,6 @@ def upload_file(task_id):
     return render_template('upload_file.html', subjects=Subject.query.all())
 
 @app.route('/uploads/<filename>')
-@login_required
 def uploaded_file(filename):
     try:
         return send_from_directory(app.config['UPLOAD_FOLDER'], filename=filename, as_attachment=False)
@@ -102,14 +93,12 @@ def uploaded_file(filename):
         abort(404)
 
 @app.route('/profile')
-@login_required
 def profile():
     user = User.query.get(current_user.id)
     image_file = url_for('static', filename='profile_pictures/' + current_user.image_file)
     return render_template('profile.html', title='Profile', user=user, image_file=image_file)
 
 @app.route('/admin_page', methods=['GET', 'POST'])
-@login_required
 def admin_page():
     user = User.query.get(current_user.id)
     if user.status != 'admin':
