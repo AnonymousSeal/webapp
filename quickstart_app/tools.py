33f7bf258b176aea432314fa818b556d007cba06
EliasKomar <elias.komar@ctemplar.com>
2020-09-19 18:38:30 +0200

    rm log, because unnecessary

--- quickstart_app/tools.py
+++ quickstart_app/tools.py
@@ -1,13 +1,4 @@
-from quickstart_app import app, db
-
-def log(text, logfile='log.txt'):
-    f = open(logfile,'a')
-    f.write(str(text) + '\n')
-    f.close()
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
-
+from quickstart_app import db, app
 from quickstart_app.models import User, Subject
 
 def make_admin(name):
@@ -17,3 +8,6 @@ def make_admin(name):
 def add_subject(name):
     db.session.add(Subject(name=name))
     db.session.commit()
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
