333847fdb9e38a411bbd8a24e0a87fbd8de7d62b
EliasKomar <elias.komar@ctemplar.com>
2020-09-19 18:41:14 +0200

    rm separate upload site

--- quickstart_app/routes.py
+++ quickstart_app/routes.py
@@ -75,29 +75,6 @@ def add_task():
         return redirect(url_for('schedule'))
     return render_template('add_task.html', title='Add Task', subjects=Subject.query.all())
 
-@app.route('/upload_file/<int:task_id>', methods=['GET', 'POST'])
-@login_required
-def upload_file(task_id):
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            db.session.add(Material(filename=filename, user_id=current_user.id, schedule_id=task_id))
-            db.session.commit()
-
-        return redirect(url_for('task', task_id=task_id))
-    return render_template('upload_file.html', title='Upload Material', subjects=Subject.query.all())
-
 @app.route('/uploads/<string:filename>')
 @login_required
 def uploaded_file(filename):
