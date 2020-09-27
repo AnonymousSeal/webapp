ffdff1814c7b5914637eaf298c8b748fb69f9746
EliasKomar <elias.komar@ctemplar.com>
2020-09-27 12:16:12 +0200

    removed useless catch

--- quickstart_app/users/forms.py
+++ quickstart_app/users/forms.py
@@ -12,20 +12,13 @@ class RegistrationForm(FlaskForm):
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
-        try:
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError('That username is taken. Please choose a different one.')
-        except:
-            pass
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        try:
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError('That email is taken. Please choose a different one.')
-        except:
-            pass
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
