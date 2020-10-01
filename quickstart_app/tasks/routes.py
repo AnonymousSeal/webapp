71b04d3ed0ee797d405bcaa40e42f21f94712352
EliasKomar <elias.komar@ctemplar.com>
2020-10-01 15:25:30 +0200

    updating and deleting comments

--- quickstart_app/tasks/routes.py
+++ quickstart_app/tasks/routes.py
@@ -23,7 +23,7 @@ def task(task_id):
     subject = Subject.query.get(task.subject_id)
     return render_template('task.html', title=task.name, task=task, subject=subject)
 
-@tasks.route('/add_comment/<int:task_id>', methods=['GET', 'POST'])
+@tasks.route('/comment/<int:task_id>', methods=['GET', 'POST'])
 @login_required
 def add_comment(task_id):
     form = CommentUploadForm()
@@ -48,13 +48,71 @@ def add_comment(task_id):
         db.session.add(comment)
         db.session.commit()
         for filename in session['file_chache']:
-            material = Material(filename=filename, upload_id=comment.id)
-            db.session.add(material)
+            db.session.add(Material(filename=filename, upload_id=comment.id))
         db.session.commit()
         session['file_chache'] = []
 
         return redirect(url_for('tasks.task', task_id=task_id))
-    return render_template('add_comment.html', title='Add Comment', form=form)
+    return render_template('comment.html', title='Add Comment',
+                            form=form, legend='Add Comment')
+
+@tasks.route('/comment/<int:comment_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_comment(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    if comment.author != current_user:
+        abort(403)
+    form = CommentUploadForm()
+
+    if not 'file_chache' in session:
+        session['file_chache'] = [material.filename for material in comment.material]
+    if not 'staged_files_location' in session:
+        session['staged_files_location'] = str(comment.task_id) + 'u' + str(comment.id)
+    if session['staged_files_location'] != str(comment.task_id) + 'u' + str(comment.id):
+        session['file_chache'] = [material.filename for material in comment.material]
+        session['staged_files_location'] = str(comment.task_id) + 'u' + str(comment.id)
+
+    if "add" in request.form and form.upload.validate(form):
+        filename = secure_filename(form.upload.upload.data.filename)
+        form.upload.upload.data.save(os.path.join(current_app.root_path, 'static/material', filename))
+        if session['file_chache'] != []:
+            session['file_chache'] = session['file_chache'] + [filename]
+        else:
+            session['file_chache'] = [filename]
+
+    if "comment" in request.form and form.comment.validate(form):
+        comment.title = form.comment.title.data
+        comment.comment = form.comment.content.data
+        for material in comment.material:
+            if material.filename not in session['file_chache']:
+                 db.session.delete(material)
+        material_filenames = [material.filename for material in comment.material]
+        for filename in session['file_chache']:
+            if filename not in material_filenames:
+                 db.session.add(Material(filename=filename, upload_id=comment.id))
+        db.session.commit()
+        return redirect(url_for('tasks.task', task_id=comment.task_id))
+
+    if request.method == 'GET':
+        form.comment.title.data = comment.title
+        form.comment.content.data = comment.comment
+
+    return render_template('comment.html', title='Update Comment',
+                            form=form, legend='Update Comment')
+
+@tasks.route('/comment/<int:comment_id>/delete', methods=['GET', 'POST'])
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    if comment.author != current_user:
+        abort(403)
+    for material in comment.material:
+        db.session.delete(material)
+    db.session.commit()
+    db.session.delete(comment)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('tasks.task', task_id=comment.task_id))
 
 @tasks.route('/add_task', methods=['GET', 'POST'])
 @login_required
