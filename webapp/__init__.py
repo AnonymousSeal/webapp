b550cee03e7cafec2cce7c2725483ce3624e36d6
EliasKomar <elias.komar@ctemplar.com>
2020-11-06 19:36:32 +0100

    main page order

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,13 +3,23 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+
+# jinja2
+def already_past(date):
+    now = datetime.now()
+    delta = date - now
+    diff = delta.days + 1
+    if diff < 0:
+        return True
+    else:
+        return False
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -21,6 +31,9 @@ def create_app(config_class=Config):
     app.register_blueprint(main)
     app.register_blueprint(tasks)
     app.register_blueprint(users)
+
+    #jinja2
+    app.jinja_env.globals.update(already_past=already_past)
 
     db.init_app(app)
     bcrypt.init_app(app)
